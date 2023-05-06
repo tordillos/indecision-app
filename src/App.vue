eddfc2ccd6851397d5dd85089b53af6f21712976
Pedro Blázquez <tordillos2@gmail.com>
2023-05-06 11:21:47 +0200

    Inicio del proecto

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Indecision />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Indecision from './components/Indecision.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Indecision
   }
 }
 </script>
